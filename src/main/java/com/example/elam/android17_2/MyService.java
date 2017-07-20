36ce32d0261d960cef2d7196b118c38cf17a2c10
Navinbala5 <bnavin28@gmail.com>
2017-07-20 13:00:24 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/android17_2/MyService.java
@@ -0,0 +1,34 @@
+package com.example.elam.android17_2;
+
+/**
+ * Created by elam on 14-07-2017.
+ */
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyService extends Service {
+
+    IBinder mBinder = new LocalBinder();
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    public class LocalBinder extends Binder {
+        public MyService getServerInstance() {
+            return MyService.this;
+        }
+    }
+
+    public String getTime() {
+        SimpleDateFormat mDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return mDateFormat.format(new Date());
+    }
+}
